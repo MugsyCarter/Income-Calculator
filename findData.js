95a7aa31cad402b9255cc17d9ed82de92004742b
Mugsy Carter <mugsycarter@gmail.com>
2016-10-10 12:06:00 -0700

    AJax call up and running.  Created dropdown menus to hold the data from the ajax call.

--- findData.js
+++ findData.js
@@ -2,19 +2,41 @@
 (function(module) {
   var census = {};
   var censusKey = '7a3aa9d2f7fafb092b5957d10b65c477719c4c4f';
-
+  var stateChoice;
 //array of all site objects that
-  census.allsites = [];
+  census.allData = [];
+
+  $('#test-button').on('click', function(){
+    census.request(alert('census.request called'));
+  });
+
+  $('#state-choice').on('change', function(){
+    stateChoice = $(this).val();
+    alert(stateChoice);
+  });
+
+  function countyFilterMaker(counties){
+    var source = $('#county-filter-template').html();
+    var template = Handlebars.compile(source);
+    var context = {county: counties};
+    var html = template(context);
+    $('#county-filter').append(html);
+  };
+
+
 
 //ajax call here
-  sites.request = function(callback) {
+  census.request = function(callback) {
     $.ajax({
       method: 'GET',
-      url: 'https://api.github.com/users/MugsyCarter/repos' + '?per_page=50&sort=updated',
+      url: 'http://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEMHI_PT,SAEPOVRTALL_PT&for=county:*&in=state:' + stateChoice + '&time=2012&key=7a3aa9d2f7fafb092b5957d10b65c477719c4c4f',
       success: function(data, status, xhr){
         console.log('ajax call was a rousing success:', data);
-        sites.allsites = data;
-        callback();
+        data.forEach(function(county){
+          console.log(county[0]);
+          countyFilterMaker(county[0]);
+        });
+        // countyFilterMaker();
       },
       error: function(xhr, settings, error){
         console.log('Ajax call failed:', error);
@@ -22,11 +44,11 @@
     });
   };
 
-  sites.withTheAttribute = function(myAttr) {
-    return sites.allsites.filter(function(aSite) {
-      return aSite[myAttr];
-    });
-  };
+  // sites.withTheAttribute = function(myAttr) {
+  //   return sites.allsites.filter(function(aSite) {
+  //     return aSite[myAttr];
+  //   });
+  // };
 
-  module.sites = sites;
+  module.census = census;
 })(window);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 5000,
   app = express();
 
 
